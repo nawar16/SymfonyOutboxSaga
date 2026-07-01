165c74bbca0e5910003dddf135191b2e21768869
nawar16 <nawarmk98@gmail.com>
2026-07-01 14:59:00 +0300

    test: add env test

---
+++ tests/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    public function testEnvironmentWorks(): void
+    {
+        $this->assertTrue(true);
+    }
+}
