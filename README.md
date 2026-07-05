994e6b9e76be47662e1ee352925648af9a0dfb68
nawar16 <nawarmk98@gmail.com>
2026-07-05 22:43:46 +0300

    feat: add migrations and readme files

---
+++ README.md
@@ -0,0 +1,4 @@
+# Event-Driven Order Processing System
+
+This project is an event-driven e-commerce backend built to explore modern distributed system concepts. 
+The goal is to implement an order processing workflow using microservices, asynchronous messaging, and patterns such as Event Sourcing, Saga, Inventory Reservation, Payments, the Outbox Pattern, and Eventual Consistency. 
---
+++ migrations/Version20260705192236.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260705192236 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE orders (id VARCHAR(36) NOT NULL, customer_id VARCHAR(36) NOT NULL, inventory_hold_id VARCHAR(36) NOT NULL, status VARCHAR(50) NOT NULL, total_amount INT NOT NULL, PRIMARY KEY (id))');
+        $this->addSql('CREATE TABLE order_items (id VARCHAR(36) NOT NULL, product_id VARCHAR(36) NOT NULL, quantity INT NOT NULL, price_in_cents INT NOT NULL, status VARCHAR(50) NOT NULL, order_id VARCHAR(36) NOT NULL, PRIMARY KEY (id))');
+        $this->addSql('CREATE INDEX IDX_62809DB08D9F6D38 ON order_items (order_id)');
+        $this->addSql('ALTER TABLE order_items ADD CONSTRAINT FK_62809DB08D9F6D38 FOREIGN KEY (order_id) REFERENCES orders (id) ON DELETE CASCADE NOT DEFERRABLE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE order_items DROP CONSTRAINT FK_62809DB08D9F6D38');
+        $this->addSql('DROP TABLE order_items');
+        $this->addSql('DROP TABLE orders');
+    }
+}
