f33a367365a1c8216ed57a604545fc97e029dfdb
nawar16 <nawarmk98@gmail.com>
2026-07-09 15:44:47 +0300

    test: text outbox message

---
+++ src/Shared/Infrastructure/Console/PublishOutboxMessagesCommand.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Shared\Infrastructure\Console;
+
+
+class PublishOutboxMessagesCommand
+{}
---
+++ tests/Ordering/Infrastructure/DoctrineOrderRepositoryTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Tests\Ordering\Infrastructure;
+
+use App\Ordering\Domain\Entity\Order;
+use App\Ordering\Domain\Entity\OrderItem;
+use App\Ordering\Domain\Repository\OrderRepositoryInterface;
+use App\Shared\Infrastructure\Persistence\OutboxMessage;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+final class DoctrineOrderRepositoryTest extends KernelTestCase
+{
+    public function testSavingOrderAlsoPersistsOutboxMessage(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        $repository = $container->get(OrderRepositoryInterface::class);
+        $entityManager = $container->get(EntityManagerInterface::class);
+        $order = Order::place('order-1','customer-1',
+            [new OrderItem('item-1','product-1',2,1000)]
+        );
+        $repository->save($order);
+        $messages = $entityManager->getRepository(OutboxMessage::class)->findAll();
+        $this->assertCount(1, $messages);
+        $this->assertNotNull($entityManager->find(Order::class, 'order-1'));
+        $this->assertCount(1,$entityManager->getRepository(OutboxMessage::class)->findAll());
+    }
+}
