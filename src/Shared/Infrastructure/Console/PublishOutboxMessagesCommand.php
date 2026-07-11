65e185abfe6d678afc789c4f11d9713b05848d30
nawar16 <nawarmk98@gmail.com>
2026-07-11 17:21:36 +0300

    feat: implement the outbox service in the publisher command

--- src/Shared/Infrastructure/Console/PublishOutboxMessagesCommand.php
+++ src/Shared/Infrastructure/Console/PublishOutboxMessagesCommand.php
@@ -2,6 +2,27 @@
 
 namespace App\Shared\Infrastructure\Console;
 
+use App\Shared\Infrastructure\Service\OutboxPublisher;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
-class PublishOutboxMessagesCommand
-{}
+#[AsCommand(
+    name: 'app:publish-outbox'
+)]
+final class PublishOutboxMessagesCommand extends Command
+{
+    public function __construct(private OutboxPublisher $publisher) 
+    {
+        parent::__construct();
+    }
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ): int 
+    {
+        $this->publisher->publishPending();
+        return Command::SUCCESS;
+    }
+}
